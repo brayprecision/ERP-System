c222d1053f01b03c2dde835f3bf820bf0aa531db
brayprecision <leland@brayprecision.com>
2026-01-18 16:02:35 -0800

    Update uninstall-bperp.bat

--- uninstall-bperp.bat
+++ uninstall-bperp.bat
@@ -2,7 +2,6 @@
 title BPERP Dashboard Uninstaller
 echo Removing BPERP Dashboard shortcuts...
 
-:: Remove desktop shortcut
 if exist "%USERPROFILE%\Desktop\BPERP Dashboard.lnk" (
     del "%USERPROFILE%\Desktop\BPERP Dashboard.lnk"
     echo [SUCCESS] Removed desktop shortcut
@@ -10,15 +9,20 @@ if exist "%USERPROFILE%\Desktop\BPERP Dashboard.lnk" (
     echo [INFO] Desktop shortcut not found
 )
 
-:: Remove start menu folder
+if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\BPERP Dashboard.lnk" (
+    del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\BPERP Dashboard.lnk"
+    echo [SUCCESS] Removed start menu shortcut
+) else (
+    echo [INFO] Start menu shortcut not found
+)
+
+:: Also remove old folder-based shortcuts if they exist
+if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Bray Precision ERP" (
+    rmdir /s /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Bray Precision ERP"
+)
 if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Bray Precision Manufacturing" (
     rmdir /s /q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Bray Precision Manufacturing"
-    echo [SUCCESS] Removed start menu shortcuts
-) else (
-    echo [INFO] Start menu shortcuts not found
 )
 
 echo BPERP Dashboard shortcuts have been removed.
-echo Installation files remain in C:\Users\user\Desktop\BPERP\
-echo.
 pause
